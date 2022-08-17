ca11d45bf6696bb09cee47cb5895224ff1fe0756
ivenckunas <ignas.venckunas@gmail.com>
2022-08-17 15:02:17 +0300

    task1 done

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,30 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+'use strict';
+console.log('script.js');
+
+
+const formEl = document.forms[0]
+const inputEl = document.getElementById('search');
+const outputEl = document.getElementById('output');
+
+formEl.addEventListener("submit", (e) => {
+    e.preventDefault()
+    createConvertedList(inputEl.value)
+})
+
+function createConvertedList(inputValue) {
+    outputEl.textContent = ""
+    outputEl.insertAdjacentHTML("afterbegin", `
+    <div class="results">
+    <h2> Results: </h2>
+    <p><span>${inputValue}</span>kg pavertus i svarus(lb) gausime <span>${(inputValue * 2.2046).toFixed(2)}</span>lb</p>
+    <p><span>${inputValue}</span>kg pavertus i gramus(g) - <span>${(inputValue / 0.0010000).toFixed(2)}</span>g</p>
+    <p><span>${inputValue}</span>kg pavertus i uncijas(oz) - <span>${(inputValue * 35.274).toFixed(2)}</span>oz</p>
+    </div>
+` )
+
+}
+
