ea8bff13522128a08015dc7c411b199099717f2e
ivenckunas <ignas.venckunas@gmail.com>
2022-08-18 10:01:09 +0300

    task 9 ES6 class constructor done

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,20 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+    constructor(title, director, budget) {
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    }
+    wasExpensive() {
+        const bud = this.budget > 100000000 ? true : false
+        console.log('Budget is more than 100 millions ===', bud);
+        return bud
+    }
+}
+
+const starWars = new Movie("A New Hope", "George Lucas", 11000000)
+console.log('starWars ===', starWars);
+starWars.wasExpensive();
