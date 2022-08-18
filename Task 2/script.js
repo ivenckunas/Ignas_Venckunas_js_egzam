437115ae46b7bfd4bc943f96b85974f58bb8aa84
ivenckunas <ignas.venckunas@gmail.com>
2022-08-18 10:28:19 +0300

    task 2 reset button added

--- Task 2/script.js
+++ Task 2/script.js
@@ -6,10 +6,16 @@ pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 'use strict';
 
 const clickBtn = document.getElementById('btn__element');
-let clickCounter = document.getElementById('btn__state');
+const clickCounter = document.getElementById('btn__state');
+const resetBtn = document.querySelector("button")
 let num = 0
 
 clickBtn.addEventListener("click", () => {
     num++
     clickCounter.textContent = num
+})
+
+resetBtn.addEventListener("click", () => {
+    num = 0
+    clickCounter.textContent = 0
 })
