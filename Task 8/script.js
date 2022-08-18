a96c01f13ac165b632e49144ef742d9ae79c59ef
ivenckunas <ignas.venckunas@gmail.com>
2022-08-18 09:38:23 +0300

    task 5 calculator function methods

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,33 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+
+function Calculator(a, b) {
+    this.sum = function () {
+        const sum = a + b
+        console.log('sum ===', sum);
+    }
+
+    this.sub = function () {
+        const sub = a - b
+        console.log('sub ===', sub);
+    }
+
+    this.mult = function () {
+        const mult = a * b
+        console.log('mult ===', mult);
+    }
+
+    this.div = function () {
+        const div = a / b
+        console.log('div ===', div);
+    }
+}
+
+
+const num = new Calculator(8, 5)
+num.sum()
+num.sub()
+num.mult()
+num.div()
